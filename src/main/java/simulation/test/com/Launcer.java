e91c6fa95d185d6e00bb584f92a7fcae991487ae
sawamk <projectpredel@gmail.com>
2024-12-11 12:45:03 +0300

    New name of class Launcher

--- src/main/java/simulation/test/com/Launcer.java
+++ src/main/java/simulation/test/com/Launcer.java
@@ -4,7 +4,7 @@ import simulation.test.com.map.Map;
 import static simulation.test.com.map.Simulation.createCurrentWorld;
 import static simulation.test.com.map.Simulation.startSimulation;
 
-public class Launcer {
+public class Launcher {
     public static void main(String[] args) {
         Map worldMap = createCurrentWorld();
         startSimulation(worldMap);
